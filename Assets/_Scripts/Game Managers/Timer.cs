ba9211de7ddf28a01ec9704227e89cf7b8b6f317
Yuuki <52652158+rene-roid@users.noreply.github.com>
2022-05-16 14:25:49 +0200

    Update Timer.cs
    Added ability to spawn blocks at random rails

--- Assets/_Scripts/Game Managers/Timer.cs
+++ Assets/_Scripts/Game Managers/Timer.cs
@@ -7,14 +7,36 @@ public class Timer : MonoBehaviour
 {
     [Header("Timer")]
     public Text TextTimer;
-    
+
+    [Header("Random Block Spawn")]
+    public GameObject Block;
+    public KeyCode spawnkey = KeyCode.E;
+
     void Start()
     {
         
     }
 
+    private void Update()
+    {
+        SpawnBlock();
+    }
+
     void FixedUpdate()
     {
         TextTimer.text = "Time: " + Mathf.Round(Time.time * 100.0f) * 0.01f;
+    }
+
+    // Spawn a random block
+    private void SpawnBlock()
+    {
+        if (Input.GetKeyDown(spawnkey))
+        {
+            GameObject newBlock = Instantiate(Block);
+            BlockController blockController = newBlock.GetComponent<BlockController>();
+            blockController.Rail = Random.Range(0, 4);
+        }
+        
+        
     }
 }
