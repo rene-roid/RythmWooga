4598c639da4225f8294a6e5b7b96ed61ab1695bf
Yuuki <52652158+rene-roid@users.noreply.github.com>
2022-05-26 15:16:47 +0200

    prepared mori scene

--- Assets/_Scripts/Camera/CameraController.cs
+++ Assets/_Scripts/Camera/CameraController.cs
@@ -18,28 +18,5 @@ public class CameraController : MonoBehaviour
     // Shake the camera
     public void Shake(float duration)
     {
-        StartCoroutine(ShakeCoroutine(duration, 358f));
-    }
-
-    // Shake the camera
-    IEnumerator ShakeCoroutine(float duration, float magnitude)
-    {
-        Vector3 originalPos = transform.localPosition;
-
-        float elapsed = 0.0f;
-
-        while (elapsed < duration)
-        {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-
-            transform.localPosition = new Vector3(x, y, originalPos.z);
-
-            elapsed += Time.deltaTime;
-
-            yield return null;
-        }
-
-        transform.position = originalPos;
     }
 }
